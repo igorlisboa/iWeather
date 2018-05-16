8e90b0624dbd882a384e56b15b6143f37059dce9
Igor Lisboa <igorlisboa34@gmail.com>
2018-05-16 14:44:54 -0300

    Chamada da api na tela home a partir do provider

--- src/providers/weather/weather.ts
+++ src/providers/weather/weather.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import 'rxjs/add/operator/map';
 
 /*
   Generated class for the WeatherProvider provider.
@@ -9,15 +10,20 @@ import { Injectable } from '@angular/core';
 */
 @Injectable()
 export class WeatherProvider {
-  apiKey: string = 'c';
+  apiKey: string = '99dfe35fcb7de1ee';
   url:string;
 
   constructor(public http: HttpClient) {
+    console.log('hello world!');
     this.url = 'http://api.wunderground.com/api/'+this.apiKey+'/conditions/q';
   }
 
   getWeather(city, state){
-    return this.http.get(this.url + '/' + state + '/' + city + '.json');
-        // .map(response => response.json());
+    console.log(this.url);
+    console.log(this.url + '/' + state + '/' + city + '.json');
+    return this.http.get(this.url + '/' + state + '/' + city + '.json')
+        .map(response => {
+          return response
+        });
   }
 }
